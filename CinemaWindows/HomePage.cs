d92ce11545c88f1c47c74f275174213e892ba98b
markhof010 <45596281+markhof010@users.noreply.github.com>
2020-05-25 15:15:25 +0200

    made a beginning

--- CinemaWindows/HomePage.cs
+++ CinemaWindows/HomePage.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace CinemaWindows
 {
-	public partial class Form1 : Form
+	public partial class HomeScreen : Form
 	{
-		public Form1()
+		public HomeScreen()
 		{
 			InitializeComponent();
+		}
+
+		private void loginButon_Click(object sender, EventArgs e)
+		{
+			InlogPage form = new InlogPage();
+			form.Show();
 		}
 	}
 }
--- CinemaWindows/Program.cs
+++ CinemaWindows/Program.cs
@@ -16,7 +16,7 @@ namespace CinemaWindows
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new HomeScreen());
 		}
 	}
 }
