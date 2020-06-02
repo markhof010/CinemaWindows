b4f3b188e73115c551ec2021d456b14a0bee346a
TheLoneWolf13 <47594026+TheLoneWolf13@users.noreply.github.com>
2020-06-02 17:00:26 +0200

    finished basic functionality of RestaurantMenu page

--- CinemaWindows/Database/GetData.cs
+++ CinemaWindows/Database/GetData.cs
@@ -10,8 +10,8 @@ using MySqlX.XDevAPI.Relational;
 
 namespace CinemaWindows.Database
 {
-	class GetData : Connecter
-	{
+    class GetData : Connecter
+    {
         public int GetHallID(int DateID)
         {
             int HallID = -1;
@@ -47,5 +47,40 @@ namespace CinemaWindows.Database
             return HallID;
         }
 
+        public List<Tuple<int, string, double>> GetProducts()
+        {
+            List<Tuple<int, string, double>> products = new List<Tuple<int, string, double>>();
+
+			try
+			{
+				Connection.Open();
+				string stringToDisplay = @"SELECT * FROM restaurantitems";
+
+                MySqlCommand command = new MySqlCommand(stringToDisplay, Connection);
+
+                MySqlDataReader dataReader = command.ExecuteReader();
+				while (dataReader.Read())
+				{
+                    int ItemID = dataReader.GetInt32("ItemID");
+                    string ItemName = dataReader["ItemName"].ToString();
+					double ItemPrice = dataReader.GetDouble("Price");
+
+                    products.Add(Tuple.Create(ItemID, ItemName, ItemPrice));
+                    //Console.WriteLine("(" + dataReader["ItemID"] + ") " + dataReader["ItemName"] + "    €" + test.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
+                }
+
+                dataReader.Close();
+                return products;
+			}
+			catch (MySqlException ex)
+			{
+
+				throw;
+			}
+			finally
+			{
+				Connection.Close();
+			}
+        }
     }
 }
--- CinemaWindows/RestaurantMenu.cs
+++ CinemaWindows/RestaurantMenu.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CinemaWindows.Database;
 
 namespace CinemaWindows
 {
@@ -15,45 +16,23 @@ namespace CinemaWindows
 		public RestaurantMenu()
 		{
 			InitializeComponent();
+			GetData GD = new GetData();			
 			this.AutoScroll = true;
 			int place = 120;
 
-			try
-			{
-				Connection.Open();
-				string stringToDisplay = @"SELECT * FROM restaurantitems";
+			for (int i = 0; i < GD.GetProducts().Count; i++)
+			{			
+				Label LB1 = new Label();
+				LB1.Text = "(" + GD.GetProducts()[i].Item1 + ") " + GD.GetProducts()[i].Item2 + "    €" + GD.GetProducts()[i].Item3.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+				LB1.Location = new Point((this.Width / 2) - 100, place);
+				LB1.Font = new Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+				LB1.AutoSize = true;
+				this.Controls.Add(LB1);
 
-				MySqlCommand command = new MySqlCommand(stringToDisplay, Connection);
-
-				MySqlDataReader dataReader = command.ExecuteReader();
-				while (dataReader.Read())
-				{
-					double test = dataReader.GetDouble("Price");
-					
-					Label LB1 = new Label();
-					LB1.Text = "(" + dataReader["ItemID"] + ") " + dataReader["ItemName"] + "    €" + test.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
-					LB1.Location = new Point((this.Width / 2) - 177, place);
-					LB1.Font = new Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-					LB1.AutoSize = true;
-					this.Controls.Add(LB1);
-
-					place += 20;
-					//Console.WriteLine("(" + dataReader["ItemID"] + ") " + dataReader["ItemName"] + "    €" + test.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
-				}
-
-				dataReader.Close();
+				place += 20;
+				//Console.WriteLine("(" + dataReader["ItemID"] + ") " + dataReader["ItemName"] + "    €" + test.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture));
 			}
-			catch (MySqlException ex)
-			{
-
-				throw;
-			}
-			finally
-			{
-				Connection.Close();
-			}
-
-
+			
 		}
 
 		private void HomeBTN_Click(object sender, EventArgs e)
