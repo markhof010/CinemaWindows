5fcf6a77e0f84b1a8f44f7d7cdaecc800dbac152
markhof010 <45596281+markhof010@users.noreply.github.com>
2020-06-02 14:25:59 +0200

    added connecter

---
+++ CinemaWindows/Database/GetData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+
+namespace CinemaWindows.Database
+{
+	class GetData : Connecter
+	{
+        public int GetHallID(int DateID)
+        {
+            int HallID = -1;
+            try
+            {
+                Connection.Open();
+                string IntToCheck = @"SELECT HallID FROM hall WHERE DateID = @DateID";
+
+                MySqlCommand command = new MySqlCommand(IntToCheck, Connection);
+                MySqlParameter DateIDParam = new MySqlParameter("@DateID", MySqlDbType.VarChar);
+
+                DateIDParam.Value = DateID;
+
+                command.Parameters.Add(DateIDParam);
+
+                MySqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    HallID = dataReader.GetInt32("HallID");
+                }
+                dataReader.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return HallID;
+        }
+
+    }
+}
