6cce69bad7178073b5819ca8113a9f73a033fac7
Sybren de Jongh <sybrendj2001@gmail.com>
2020-06-03 14:50:29 +0200

    Added login page

--- CinemaWindows/InlogPage.Designer.cs
+++ CinemaWindows/InlogPage.Designer.cs
@@ -28,19 +28,78 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// InlogPage
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Name = "InlogPage";
-			this.Text = "Login Page";
-			this.ResumeLayout(false);
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoginButton.Location = new System.Drawing.Point(150, 200);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(100, 25);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UsernameTextBox.Location = new System.Drawing.Point(100, 100);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(200, 22);
+            this.UsernameTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PasswordTextBox.Location = new System.Drawing.Point(100, 150);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(200, 22);
+            this.PasswordTextBox.TabIndex = 2;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(130, 50);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(145, 17);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.Text = "Type your credentials";
+            // 
+            // InlogPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 264);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.LoginButton);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "InlogPage";
+            this.Text = "Login Page";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
-	}
+
+		private System.Windows.Forms.Button LoginButton;
+		private System.Windows.Forms.TextBox UsernameTextBox;
+		private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label infoLabel;
+    }
 }
--- CinemaWindows/InlogPage.cs
+++ CinemaWindows/InlogPage.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace CinemaWindows
 {
 	public partial class InlogPage : Form
 	{
-		public InlogPage()
+        public static string sha256_hash(string valueToEncrypt)
+        {
+            StringBuilder stringbuilder = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(valueToEncrypt));
+
+                foreach (byte b in result)
+                {
+                    stringbuilder.Append(b.ToString());
+                }
+            }
+            return stringbuilder.ToString();
+        }
+
+        public InlogPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            bool checkLogin = true;
+            SHA256 encrypter = SHA256.Create();
+            ChangeData logincheck = new ChangeData();
+            string encryptedusername = sha256_hash(UsernameTextBox.Text);
+            string encryptedpassword = sha256_hash(PasswordTextBox.Text);
+
+            string checkeddata = logincheck.checkLoginAndFunction(encryptedusername, encryptedpassword);
+
+            switch (checkeddata)
+            {
+                case "admin":
+                    //send to admin page
+                    this.Close();
+                    break;
+
+                case "retailer":
+                    //send to retailer page
+                    this.Close();
+                    break;
+
+                case "ticketsalesman":
+                    //send to ticketsaleman page
+                    this.Close();
+                    break;
+
+                default:
+                    infoLabel.Text = "These are the wrong cridentials";
+                    break;
+            }
+        }
+    }
 }
