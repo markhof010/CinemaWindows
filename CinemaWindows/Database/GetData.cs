6d1b38f784e05a7b26908a8b2c5234c17b24f63f
markhof010 <45596281+markhof010@users.noreply.github.com>
2020-06-04 16:02:49 +0200

    added the check if available

--- CinemaWindows/Database/GetData.cs
+++ CinemaWindows/Database/GetData.cs
@@ -47,5 +47,95 @@ namespace CinemaWindows.Database
             return HallID;
         }
 
+        public Tuple<int, int, int, int, double, double, double> GetHallInfo(int DateID)
+        {
+            int row = 0;
+            int col = 0;
+            int HallID = 0;
+            double inner = 0;
+            double middle = 0;
+            double outer = 0;
+
+            try
+            {
+                Connection.Open();
+                string IntToCheck = @"SELECT * FROM hall WHERE DateID = @DateID";
+
+                MySqlCommand command = new MySqlCommand(IntToCheck, Connection);
+                MySqlParameter DateIDParam = new MySqlParameter("@DateID", MySqlDbType.Int32);
+
+                DateIDParam.Value = DateID;
+
+                command.Parameters.Add(DateIDParam);
+
+                MySqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    row = dataReader.GetInt32("RowLength");
+                    col = dataReader.GetInt32("ColLength");
+                    HallID = dataReader.GetInt32("HallID");
+                    inner = dataReader.GetDouble("InnerCircle");
+                    middle = dataReader.GetDouble("MiddleCircle");
+                    outer = dataReader.GetDouble("OuterCircle");
+                }
+                dataReader.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+            return Tuple.Create(row, col, DateID, HallID, inner, middle, outer);
+        }
+
+        public List<Tuple<double, int, int, string, bool>> GetSeat(int HallID)
+        {
+            List<Tuple<double, int, int, string, bool>> seat = new List<Tuple<double, int, int, string, bool>>();
+            try
+            {
+                Connection.Open();
+                string IntToCheck = @"SELECT * FROM seats WHERE HallID = @HallID";
+
+                MySqlCommand command = new MySqlCommand(IntToCheck, Connection);
+                MySqlParameter HallIDParam = new MySqlParameter("@HallID", MySqlDbType.Int32);
+
+                HallIDParam.Value = HallID;
+
+                command.Parameters.Add(HallIDParam);
+
+
+                MySqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    double price = dataReader.GetDouble("price");
+                    int Y = dataReader.GetInt32("RowSeat");
+                    int X = dataReader.GetInt32("ColumnSeat");
+                    string Name = dataReader.GetString("Name");
+                    bool avail = false;
+                    if (dataReader.GetInt32("Availability") == 1)
+                    {
+                        avail = true;
+                    }
+
+                    seat.Add(Tuple.Create(price, Y, X, Name, avail));
+                }
+                dataReader.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return seat;
+        }
     }
 }
